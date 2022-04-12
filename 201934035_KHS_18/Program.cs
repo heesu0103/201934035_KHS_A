eb74d7aec3142519396ffe0d4e1e9b10ba450ab1
rhtmaehcl0103@naver.com <rhtmaehcl0103@naver.com>
2022-04-13 06:44:26 +0900

    초기 커밋.

---
+++ 201934035_KHS_18/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _201934035_KHS_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sign = false; ;
+            double pi = 0;
+
+            for (int i = 1; i <= 10000; i += 2)
+            {
+                if (sign == false)
+                {
+                    pi += 1.0 / i;
+                    sign = true;
+                }
+                else
+                {
+                    pi -= 1.0 / i;
+                    sign = false;
+                }
+                Console.WriteLine("i = {0}, PI = {1}", i, 4 * pi);
+            }
+            Console.ReadLine();
+        }
+    }
+}
